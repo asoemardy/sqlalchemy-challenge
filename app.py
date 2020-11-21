b83107a8086a57c5054c682e74c47dd92d9020b5
Falefor <68723120+asoemardy@users.noreply.github.com>
2020-11-21 15:30:16 -0800

    Homework Done

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 #################################################
@@ -40,8 +40,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/&ltstart&gt<br/>"
+        f"/api/v1.0/&ltstart&gt/&ltend&gt"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -50,14 +50,20 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of precipitations for the past year"""
-    # Query all passengers
+    # Query all precipitation list
     results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= '2016-08-23').all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    one_year_prcp = list(np.ravel(results))
+# Create a dictionary from the row data and append to a list of one year prcp
+    one_year_prcp = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        one_year_prcp.append(prcp_dict)
+
 
     return jsonify(one_year_prcp)
 
@@ -67,7 +73,7 @@ def stations():
     session = Session(engine)
 
     """Return a list of weather stations"""
-    # Query all passengers
+    # Query all stations
     results = session.query(Station.station).all()
 
     session.close()
@@ -83,17 +89,64 @@ def tobs():
     session = Session(engine)
 
     """Return a list of temperature observation for the most active station in the last 1 year of data"""
-    # Query all passengers
+    # Query all temperature observation
     results = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
         filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-18').\
         order_by(Measurement.date.desc()).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    tobs_list = list(np.ravel(results))
+ # Create a dictionary from the row data and append to a list of tobs_list
+    tobs_list = []
+    for station, date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["station"] = station
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_list.append(tobs_dict)
+
 
     return jsonify(tobs_list)
+
+@app.route("/api/v1.0/<start>")
+def timestart(start):
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        group_by(Measurement.date).all()
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of tobs_list
+    time_list = []
+    for result in results:
+        time_dict = {}
+        time_dict["date"] = result[0]
+        time_dict["TMIN"] = result[1]
+        time_dict["TMAX"] = result[2]
+        time_dict["TAVG"] = result[3]
+        time_list.append(time_dict)
+
+    return jsonify(time_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def timerange(start, end):
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).\
+        group_by(Measurement.date).all()
+    session.close()
+
+ # Create a dictionary from the row data and append to a list of tobs_list
+    time_list = []
+    for result in results:
+        time_dict = {}
+        time_dict["date"] = result[0]
+        time_dict["TMIN"] = result[1]
+        time_dict["TMAX"] = result[2]
+        time_dict["TAVG"] = result[3]
+        time_list.append(time_dict)
+
+    return jsonify(time_list)
 
 
 
